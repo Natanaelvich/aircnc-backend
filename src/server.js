855fc73584b272d582ce29967bbb88bc043b74a0
Natanael Lima <taelima1997@gmail.com>
2020-02-07 00:34:11 -0300

    heroku  um cu

--- src/server.js
+++ src/server.js
@@ -20,6 +20,16 @@ mongoose
   })
 
 server.use(cors())
+server.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Credentials', true)
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json'
+  )
+  next()
+})
 server.use(express.json())
 server.use(routes)
 
