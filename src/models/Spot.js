a38f855bf462c4076f1254e4ef764e4478d97e26
Natanael Lima <taelima1997@gmail.com>
2020-02-07 10:50:04 -0300

    mongoose virtuals true

--- src/models/Spot.js
+++ src/models/Spot.js
@@ -1,16 +1,23 @@
 const { Schema } = require('mongoose')
 const mongoose = require('mongoose')
 
-const SpotSchema = new Schema({
-  thumbnail: String,
-  company: String,
-  price: Number,
-  techs: [String],
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'users',
+const SpotSchema = new Schema(
+  {
+    thumbnail: String,
+    company: String,
+    price: Number,
+    techs: [String],
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'users',
+    },
   },
-})
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+)
 
 SpotSchema.virtual('thumbnail_url').get(function() {
   return `https://aircnc-backed.herokuapp.com/files/${this.thumbnail}`
